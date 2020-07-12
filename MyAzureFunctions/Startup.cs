9293e352ca350a066ce69f2a35c06d1dc813281c
damienbod <damien_bod@hotmail.com>
2020-07-12 11:48:20 +0200

    Using Azure key vault for deployment

--- MyAzureFunctions/Startup.cs
+++ MyAzureFunctions/Startup.cs
@@ -1,10 +1,11 @@
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyAzureFunctions;
 using MyAzureFunctions.Activities;
 using System;
-using System.Configuration;
 using System.Reflection;
 
 [assembly: FunctionsStartup(typeof(Startup))]
@@ -15,16 +16,30 @@ namespace MyAzureFunctions
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            var config = new ConfigurationBuilder()
+            var keyVaultEndpoint = Environment.GetEnvironmentVariable("AzureKeyVaultEndpoint");
+            if (!string.IsNullOrEmpty(keyVaultEndpoint))
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                var config = new ConfigurationBuilder()
+                    .AddAzureKeyVault(keyVaultEndpoint)
+                    .Build();
+
+                builder.Services.AddSingleton<IConfiguration>(config);
+            }
+            else
+            {
+                var config = new ConfigurationBuilder()
                .SetBasePath(Environment.CurrentDirectory)
                .AddJsonFile("local.settings.json", true)
                .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
                .AddEnvironmentVariables()
                .Build();
 
-            builder.Services.AddSingleton<IConfiguration>(config);
-
-            //var tableStorageConnection = Environment.GetEnvironmentVariable("connection...");        
+                builder.Services.AddSingleton<IConfiguration>(config);
+            }
+      
             builder.Services.AddScoped<MyActivities>();
 
             builder.Services.AddOptions<MyConfiguration>()
