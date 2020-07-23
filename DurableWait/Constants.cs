9b8ec55368a57fe0e2c0754f45d9de19fd4f2c31
damienbod <damien_bod@hotmail.com>
2020-07-23 16:19:17 +0200

    renaming external input durable function example

---
+++ DurableWait/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace MyAzureFunctions
+{
+    public static class Constants
+    {
+        public const string MyOrchestration = "MyOrchestration";
+        public const string MyActivityOne = "MyActivityOne";
+        public const string MyActivityTwo = "MyActivityTwo";
+
+        public const string MyExternalInputEvent = "MyExternalInputEvent";
+
+        public const string BeginFlowWithHttpPost = "BeginFlowWithHttpPost";
+        public const string ExternalHttpPostInput = "ExternalHttpPostInput";
+    }
+}
---
+++ DurableWait/Orchestrations/MyOrchestration.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using MyAzureFunctions.Model;
+
+namespace MyAzureFunctions.Orchestrations
+{
+    public class MyOrchestration
+    {
+        [FunctionName(Constants.MyOrchestration)]
+        public async Task<MyOrchestrationDto> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            var myOrchestrationDto = new MyOrchestrationDto
+            {
+                InputStartData = context.GetInput<string>()
+            };
+
+            if (!context.IsReplaying)
+            {
+                log.LogWarning($"begin MyOrchestration with input {context.GetInput<string>()}");
+            }
+
+            var myActivityOne = await context.CallActivityAsync<string>(
+                Constants.MyActivityOne, context.GetInput<string>());
+
+            myOrchestrationDto.MyActivityOneResult = myActivityOne;
+
+            if(!context.IsReplaying)
+            {
+                log.LogWarning($"myActivityOne completed {myActivityOne}");
+            }
+
+            var myActivityTwoInputEvent = await context.WaitForExternalEvent<string>(
+                Constants.MyExternalInputEvent);
+            myOrchestrationDto.ExternalInputData = myActivityTwoInputEvent;
+
+            var myActivityTwo = await context.CallActivityAsync<string>(
+                Constants.MyActivityTwo, myActivityTwoInputEvent);
+
+            myOrchestrationDto.MyActivityTwoResult = myActivityTwo;
+
+            if (!context.IsReplaying)
+            {
+                log.LogWarning($"myActivityTwo completed {myActivityTwo}");
+            }
+
+            return myOrchestrationDto;
+        }
+    }
+}
