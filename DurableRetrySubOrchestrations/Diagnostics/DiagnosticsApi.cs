0f1b76382a8508a28b109405a85578d6a6c3c506
damienbod <damien_bod@hotmail.com>
2025-11-21 23:08:21 +0100

    * 'Line 8: Added the using directive for Microsoft.DurableTask.Client to resolve OrchestrationRuntimeStatus.' in file 'DurableRetrySubOrchestrations\Diagnostics\DiagnosticsApi.cs'

--- DurableRetrySubOrchestrations/Diagnostics/DiagnosticsApi.cs
+++ DurableRetrySubOrchestrations/Diagnostics/DiagnosticsApi.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask.Client;
 
 namespace DurableRetrySubOrchestrations.Diagnostics
 {
