dacbecaa46936c1f6051a4109c16ca96f4d4dbeb
damienbod <damien_bod@hotmail.com>
2025-11-22 08:52:27 +0100

    Update to latest Azure functions and V4

--- DurableExternal/Program.cs
+++ DurableExternal/Program.cs
@@ -1,54 +1,61 @@
 using Azure.Identity;
+using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyAzureFunctions;
 using MyAzureFunctions.Activities;
-using System.Reflection;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication() // Using ASP.NET Core integration
-    .ConfigureServices((context, services) =>
+using Microsoft.Azure.Functions.Worker;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddScoped<MyActivities>();
+
+builder.Services.AddOptions<MyConfiguration>()
+    .Configure<IConfiguration>((settings, configuration) =>
     {
-        // Migrated from Startup.cs
-        services.AddScoped<MyActivities>();
+        configuration.GetSection("MyConfiguration").Bind(settings);
+    });
 
-        services.AddOptions<MyConfiguration>()
-            .Configure<IConfiguration>((settings, configuration) =>
-            {
-                configuration.GetSection("MyConfiguration").Bind(settings);
-            });
-
-        services.AddOptions<MyConfigurationSecrets>()
-            .Configure<IConfiguration>((settings, configuration) =>
-            {
-                configuration.GetSection("MyConfigurationSecrets").Bind(settings);
-            });
-    })
-    .ConfigureAppConfiguration((context, configBuilder) =>
+builder.Services.AddOptions<MyConfigurationSecrets>()
+    .Configure<IConfiguration>((settings, configuration) =>
     {
-        var builtConfig = configBuilder.Build();
-        var keyVaultEndpoint = builtConfig["AzureKeyVaultEndpoint"];
+        configuration.GetSection("MyConfigurationSecrets").Bind(settings);
+    });
 
-        if (!string.IsNullOrEmpty(keyVaultEndpoint))
-        {
-            // using Key Vault, either local dev or deployed
-            configBuilder
-                .SetBasePath(Environment.CurrentDirectory)
-                .AddAzureKeyVault(new Uri(keyVaultEndpoint), new DefaultAzureCredential())
-                .AddJsonFile("local.settings.json", optional: true)
-                .AddEnvironmentVariables();
-        }
-        else
-        {
-            // local dev no Key Vault
-            configBuilder
-                .SetBasePath(Environment.CurrentDirectory)
-                .AddJsonFile("local.settings.json", optional: true)
-                .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true)
-                .AddEnvironmentVariables();
-        }
-    })
-    .Build();
 
-host.Run();
+    //builder..ConfigureAppConfiguration((context, configBuilder) =>
+    //{
+    //    var builtConfig = configBuilder.Build();
+    //    var keyVaultEndpoint = builtConfig["AzureKeyVaultEndpoint"];
+
+    //    if (!string.IsNullOrEmpty(keyVaultEndpoint))
+    //    {
+    //        // using Key Vault, either local dev or deployed
+    //        configBuilder
+    //            .SetBasePath(Environment.CurrentDirectory)
+    //            .AddAzureKeyVault(new Uri(keyVaultEndpoint), new DefaultAzureCredential())
+    //            .AddJsonFile("local.settings.json", optional: true)
+    //            .AddEnvironmentVariables();
+    //    }
+    //    else
+    //    {
+    //        // local dev no Key Vault
+    //        configBuilder
+    //            .SetBasePath(Environment.CurrentDirectory)
+    //            .AddJsonFile("local.settings.json", optional: true)
+    //            .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true)
+    //            .AddEnvironmentVariables();
+    //    }
+    //})
+    //.Build();
+
+builder.Build().Run();
+
