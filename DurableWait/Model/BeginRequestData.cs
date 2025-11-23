598f9d3dec636f6e1410461363b79316f965a80c
damienbod <damien_bod@hotmail.com>
2025-11-23 08:24:33 +0100

    Fix serialization

--- DurableWait/Model/BeginRequestData.cs
+++ DurableWait/Model/BeginRequestData.cs
@@ -1,7 +1,11 @@
-﻿namespace DurableWait.Model;
+﻿using System.Text.Json.Serialization;
+
+namespace DurableWait.Model;
 
 public class BeginRequestData
 {
+    [JsonPropertyName("id")]
     public string Id { get; set; }
+    [JsonPropertyName("message")]
     public string Message { get; set; }
 }
